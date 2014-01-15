3181440932c3f2a9a16da8b5caac2f92cbe4d724
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2014-01-14 18:52:14 -0600

    Tech Debt: completed required information in setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ from setuptools import setup, find_packages
 setup(
     name="scaffold-vim-plugin",
     version='0.0.1',
-    description="",  # TODO
+    description="A tool that creates a scafold for Vim plugins to be written in Python.",
     long_description=file('README.md').read(),
     author="Jarrod C. Taylor",
-    author_email="",  # TODO
-    url="",  # TODO
+    author_email="jarrod.c.taylor@gmail.com",
+    url="http://github.com/JarrodCTaylor/vim-plugin-starter-kit",
+    license='MIT',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     classifiers=[
-        # TODO https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Topic :: Text Editors",
+        'Intended Audience :: Developers',
     ],
     tests_require=file('requirements.txt').read().split('\n'),
     test_suite='nose.collector',
